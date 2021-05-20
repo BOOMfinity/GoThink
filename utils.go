4e2e6fea77c82b370b1865097752b317266314c1
VenomPCPL <venompcpl@gmail.com>
2021-05-20 20:39:07 +0200

    fix: remove unused types

--- utils.go
+++ utils.go
@@ -28,20 +28,3 @@ func ReadableByteCount(b uint64) string {
 	return fmt.Sprintf("%.2f %ciB",
 		float64(b)/float64(div), "KMGTPE"[exp])
 }
-
-type DatabaseFlags struct {
-	Host         string `long:"host" description:"RethinkDB address" default:"localhost"`
-	Password     string `long:"pass" description:"RethinkDB admin user password"`
-	PasswordFile string `long:"pass-file" description:"Path to the file with password"`
-	Port         uint   `long:"port" description:"RethinkDB client driver port" default:"28015"`
-}
-
-type ExportFlags struct {
-	Path string `long:"export" description:"What will be exported. Use database.table syntax."`
-	File string `long:"file" description:"Backup filename or path" default:"backup.tar.gz"`
-}
-
-type ImportFlags struct {
-	Import string `long:"import" description:"Use database.table syntax"`
-	File   string `long:"file" description:"Backup filename or path" default:"backup.tar.gz"`
-}
