68f6570372177b0b01b0678552b6acb0db45a713
VenomPCPL <venompcpl@gmail.com>
2021-05-22 20:33:20 +0200

    build: v1.0.1

--- consts.go
+++ consts.go
@@ -3,9 +3,9 @@ package GoThink
 import "github.com/hashicorp/go-version"
 
 const (
-	Version = "1.0.0"
+	Version = "1.0.1"
 )
 
 var (
-	Supported, _ = version.NewConstraint("<=1.0.0")
+	Supported, _ = version.NewConstraint("<=1.0.1")
 )
